b46b6209caf0fb81c8193d7d87fa92dd237c4513
Roey Borsteinas <roeyb1@hotmail.com>
2024-03-24 21:55:12 -0400

    Implemented sprite drawing (still not functional due to memory bug)

--- assets/shaders/source/sprite_vs.hlsl
+++ assets/shaders/source/sprite_vs.hlsl
@@ -1,7 +1,7 @@
 #include "bindless.hlsli"
 
 struct VSInput {
-    float3 vertex_pos: VERTEX_POSITION;
+    float2 vertex_pos: VERTEX_POSITION;
     float2 tex_coord: TEXCOORD;
     float3 instance_pos: INSTANCE_POS;
     float2 instance_scale: INSTANCE_SCALE;
@@ -24,8 +24,8 @@ ConstantBuffer<PassConstants> g_pass_consts : REGISTER_CBV(0, 0, 0);
 VSOutput main(VSInput input) {
     VSOutput output = (VSOutput)0;
 
-    float3 vertex_pos = input.vertex_pos;
-    float3 scaled_pos = vertex_pos * float3(input.instance_scale, 1.0);
+    float2 vertex_pos = input.vertex_pos;
+    float3 scaled_pos = float3(vertex_pos, 1.) * float3(input.instance_scale, 1.0);
 
     output.position = mul(g_pass_consts.view_projection_matrix, float4(scaled_pos + input.instance_pos - g_pass_consts.camera_position, 1.));
     output.tex_coord = input.tex_coord;
