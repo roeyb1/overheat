29b5c09b198465c21dbd2ee1bdbbefefa782ab47
Roey Borsteinas <roeyb1@hotmail.com>
2024-05-07 21:20:28 -0400

    Fixed missing sort in sprite instance batching

--- assets/shaders/source/sprite_vs.hlsl
+++ assets/shaders/source/sprite_vs.hlsl
@@ -3,7 +3,7 @@
 struct VSInput {
     float2 vertex_pos: VERTEX_POSITION;
     float2 tex_coord: TEXCOORD;
-    float3 instance_pos: INSTANCE_POS;
+    float2 instance_pos: INSTANCE_POS;
     float2 instance_scale: INSTANCE_SCALE;
     uint sprite_index: SPRITE_INDEX;
 };
@@ -26,7 +26,7 @@ VSOutput main(VSInput input) {
 
     float2 vertex_pos = input.vertex_pos;
     float3 scaled_vertex_pos = float3(vertex_pos, 1.) * float3(input.instance_scale, 1);
-    float3 worlspace_pos = input.instance_pos - g_pass_consts.camera_position;
+    float3 worlspace_pos = float3(input.instance_pos, 0.) - g_pass_consts.camera_position;
 
     output.position = mul(g_pass_consts.view_projection_matrix, float4((scaled_vertex_pos + worlspace_pos).xy, 0, 1.));
     output.tex_coord = input.tex_coord;
