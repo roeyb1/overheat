fb257ba506cc70ded086b95e14bfa56187614dc8
Roey Borsteinas <roeyb1@hotmail.com>
2024-05-18 20:22:37 -0400

    Implemented a lightmap render pipeline

---
+++ assets/shaders/source/lightmap_ps.hlsl
@@ -0,0 +1,39 @@
+#include "bindless.hlsli"
+
+struct PSInput {
+    float4 position: SV_Position;
+    float2 tex_coord: TEXCOORD;
+    float4 color: COLOR;
+    float intensity: INTENSITY;
+};
+
+struct PSOutput {
+    float4 color: SV_Target0;
+};
+
+struct PassConstants {
+    float4x4 view_projection_matrix;
+    float3 camera_position;
+};
+
+struct DrawConstants {
+    uint point_light_texture;
+};
+
+ConstantBuffer<PassConstants> g_pass_consts : REGISTER_CBV(0, 0, 0);
+
+Texture2D<float4> g_textures[65536] : REGISTER_SRV(0, 0, 1);
+
+SamplerState g_aniso_repeat_sampler : REGISTER_SAMPLER(0, 0, 2);
+
+PUSH_CONSTS(DrawConstants, g_draw_consts);
+
+PSOutput main(PSInput input) {
+    PSOutput output;
+
+    const uint light_texture_index = uint(g_draw_consts.point_light_texture);
+
+    output.color = input.color * input.intensity * g_textures[light_texture_index].SampleLevel(g_aniso_repeat_sampler, input.tex_coord, 0);
+
+    return output;
+}
--- assets/shaders/source/lightmap_vs.hlsl
+++ assets/shaders/source/lightmap_vs.hlsl
@@ -1,17 +1,21 @@
+
 #include "bindless.hlsli"
 
 struct VSInput {
     float2 vertex_pos: VERTEX_POSITION;
     float2 tex_coord: TEXCOORD;
+
+    float4 color: COLOR;
     float2 instance_pos: INSTANCE_POS;
-    float2 instance_scale: INSTANCE_SCALE;
-    uint sprite_index: SPRITE_INDEX;
+    float radius: RADIUS;
+    float intensity: INTENSITY;
 };
 
 struct VSOutput {
     float4 position: SV_Position;
     float2 tex_coord: TEXCOORD;
-    uint sprite_index: SPRITE_INDEX;
+    float4 color: COLOR;
+    float intensity: INTENSITY;
 };
 
 struct PassConstants {
@@ -25,12 +29,13 @@ VSOutput main(VSInput input) {
     VSOutput output = (VSOutput)0;
 
     float2 vertex_pos = input.vertex_pos;
-    float3 scaled_vertex_pos = float3(vertex_pos, 1.) * float3(input.instance_scale, 1);
+    float3 scaled_vertex_pos = float3(vertex_pos, 1.) * float3(input.radius, input.radius, 1);
     float3 worlspace_pos = float3(input.instance_pos, 0.) - g_pass_consts.camera_position;
 
     output.position = mul(g_pass_consts.view_projection_matrix, float4((scaled_vertex_pos + worlspace_pos).xy, 0, 1.));
     output.tex_coord = input.tex_coord;
-    output.sprite_index = input.sprite_index;
+    output.color = input.color;
+    output.intensity = input.intensity;
 
     return output;
 }
