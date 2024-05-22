29aada6b66d047af13b3d34d6c52343043467a4d
Roey Borsteinas <roeyb1@hotmail.com>
2024-05-21 20:07:31 -0400

    Set up light drawing to also handle clearing alpha

--- assets/shaders/source/lightmap_ps.hlsl
+++ assets/shaders/source/lightmap_ps.hlsl
@@ -3,7 +3,7 @@
 struct PSInput {
     float4 position: SV_Position;
     float2 tex_coord: TEXCOORD;
-    float4 color: COLOR;
+    float3 color: COLOR;
     float intensity: INTENSITY;
 };
 
@@ -24,7 +24,7 @@ ConstantBuffer<PassConstants> g_pass_consts : REGISTER_CBV(0, 0, 0);
 
 Texture2D<float4> g_textures[65536] : REGISTER_SRV(0, 0, 1);
 
-SamplerState g_aniso_repeat_sampler : REGISTER_SAMPLER(0, 0, 2);
+SamplerState g_point_clamp_sampler : REGISTER_SAMPLER(0, 0, 2);
 
 PUSH_CONSTS(DrawConstants, g_draw_consts);
 
@@ -33,7 +33,9 @@ PSOutput main(PSInput input) {
 
     const uint light_texture_index = uint(g_draw_consts.point_light_texture);
 
-    output.color = input.color * input.intensity * g_textures[light_texture_index].SampleLevel(g_aniso_repeat_sampler, input.tex_coord, 0);
+    output.color.xyz = input.color.xyz * input.intensity * g_textures[light_texture_index].SampleLevel(g_point_clamp_sampler, input.tex_coord, 0).xyz;
+    // clear the alpha back to 1 so we can re-draw shadowmaps for the next light
+    output.color.a = 1.;
 
     return output;
 }
--- assets/shaders/source/lightmap_vs.hlsl
+++ assets/shaders/source/lightmap_vs.hlsl
@@ -28,12 +28,16 @@ ConstantBuffer<PassConstants> g_pass_consts : REGISTER_CBV(0, 0, 0);
 VSOutput main(VSInput input) {
     VSOutput output = (VSOutput)0;
 
-    float2 vertex_pos = input.vertex_pos;
+    // Scale up the quads to ensure they cover the entire render space so we can use the pixel shader to also reset the alpha mask
+    float2 screenspace_scale = float2(16.f, 16.f);
+    float2 vertex_pos = input.vertex_pos * screenspace_scale;
     float3 scaled_vertex_pos = float3(vertex_pos, 1.) * float3(input.radius, input.radius, 1);
     float3 worlspace_pos = float3(input.instance_pos, 0.) - g_pass_consts.camera_position;
 
-    output.position = mul(g_pass_consts.view_projection_matrix, float4((scaled_vertex_pos + worlspace_pos).xy, 0, 1.));
-    output.tex_coord = input.tex_coord;
+    output.position = (mul(g_pass_consts.view_projection_matrix, float4((scaled_vertex_pos + worlspace_pos).xy, 0, 1.)));
+
+    // Remap the texture coordinates to the unscaled space
+    output.tex_coord = (input.tex_coord - float2(0.5, 0.5)) * (screenspace_scale) + float2(0.5, 0.5);
     output.color = input.color;
     output.intensity = input.intensity;
 
