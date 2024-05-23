181e3f13e77de87018428ac838244a73bc1df332
Roey Borsteinas <roeyb1@hotmail.com>
2024-05-23 16:55:04 -0400

    Shadows (#13)
    * Implemented pipeline for shadowmap rendering
    
    * Set up light drawing to also handle clearing alpha
    
    * Reworked the light/shadow passes to work for individual lights
    
    * Shadowmap rendering properly implemented
    
    * Moved ambient color to code and added shadow caster component
    
    * Presubmit cleanup
    
    * Removed duplicate instances of quad vertex buffers
    
    quad geometry is needed in almost all render passes. Consolidate it into a global immutable device local buffer instead of reallocing and transfering it each frame for every render pass.
    
    * Implemented scissor rect to optimize shadowmap drawing
    
    * Use homogenous coordinate trick to extent shadow segments to infinity

--- assets/shaders/source/lightmap_vs.hlsl
+++ assets/shaders/source/lightmap_vs.hlsl
@@ -4,18 +4,11 @@
 struct VSInput {
     float2 vertex_pos: VERTEX_POSITION;
     float2 tex_coord: TEXCOORD;
-
-    float4 color: COLOR;
-    float2 instance_pos: INSTANCE_POS;
-    float radius: RADIUS;
-    float intensity: INTENSITY;
 };
 
 struct VSOutput {
     float4 position: SV_Position;
     float2 tex_coord: TEXCOORD;
-    float4 color: COLOR;
-    float intensity: INTENSITY;
 };
 
 struct PassConstants {
@@ -23,19 +16,32 @@ struct PassConstants {
     float3 camera_position;
 };
 
+struct DrawConstants {
+    float4 color;
+    float2 position;
+    float radius;
+    float intensity;
+
+    uint point_light_texture;
+};
+
 ConstantBuffer<PassConstants> g_pass_consts : REGISTER_CBV(0, 0, 0);
+
+PUSH_CONSTS(DrawConstants, g_draw_consts);
 
 VSOutput main(VSInput input) {
     VSOutput output = (VSOutput)0;
 
-    float2 vertex_pos = input.vertex_pos;
-    float3 scaled_vertex_pos = float3(vertex_pos, 1.) * float3(input.radius, input.radius, 1);
-    float3 worlspace_pos = float3(input.instance_pos, 0.) - g_pass_consts.camera_position;
+    // Scale up the quads to ensure they cover the entire render space so we can use the pixel shader to also reset the alpha mask
+    float2 screenspace_scale = float2(100.f, 100.f);
+    float2 vertex_pos = input.vertex_pos * screenspace_scale;
+    float3 scaled_vertex_pos = float3(vertex_pos, 1.) * float3(g_draw_consts.radius, g_draw_consts.radius, 1);
+    float3 worlspace_pos = float3(g_draw_consts.position, 0.) - g_pass_consts.camera_position;
 
-    output.position = mul(g_pass_consts.view_projection_matrix, float4((scaled_vertex_pos + worlspace_pos).xy, 0, 1.));
-    output.tex_coord = input.tex_coord;
-    output.color = input.color;
-    output.intensity = input.intensity;
+    output.position = (mul(g_pass_consts.view_projection_matrix, float4((scaled_vertex_pos + worlspace_pos).xy, 0, 1.)));
+
+    // Remap the texture coordinates to the unscaled space
+    output.tex_coord = (input.tex_coord - float2(0.5, 0.5)) * (screenspace_scale) + float2(0.5, 0.5);
 
     return output;
 }
