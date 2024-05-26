95ab22c1275335c83cbf96483c582a587aee445f
Roey Borsteinas <roeyb1@hotmail.com>
2024-05-26 14:49:52 -0400

    Layers above the background now receive lighting but no shadows

--- assets/shaders/source/apply_lighting_cs.hlsl
+++ assets/shaders/source/apply_lighting_cs.hlsl
@@ -16,11 +16,15 @@ PUSH_CONSTS(PushConsts, g_push_consts);
 [numthreads(8, 8, 1)]
 void main(uint3 thread_id: SV_DispatchThreadID) {
 
-    float3 scene_color = g_textures[g_push_consts.input_color_image_index].Load(int3(thread_id.xy, 0)).rgb;
+    float4 scene_color = g_textures[g_push_consts.input_color_image_index].Load(int3(thread_id.xy, 0));
 
-    float3 light_color = g_textures[g_push_consts.input_lightmap_image_index].Load(int3(thread_id.xy, 0)).rgb;
+    // we don't want the alpha of the lightmap to affect the blending in this stage, so set it to 1
+    float4 light_color = float4(g_textures[g_push_consts.input_lightmap_image_index].Load(int3(thread_id.xy, 0)).rgb, 1.0);
 
-    float3 color = scene_color * light_color;
+    float4 src_color = scene_color * light_color;
+    float4 dst_color = g_rw_textures[g_push_consts.output_image_index][thread_id.xy];
 
-    g_rw_textures[g_push_consts.output_image_index][thread_id.xy] = float4(color, 1.f);
+    float4 output_color = dst_color * (1. - src_color.a) + src_color * src_color.a;
+
+    g_rw_textures[g_push_consts.output_image_index][thread_id.xy] = output_color;
 }
