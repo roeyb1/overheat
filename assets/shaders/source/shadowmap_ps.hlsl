66782e5c5bea3f8680ebb96235cc0e5d3b186689
Roey Borsteinas <roeyb1@hotmail.com>
2024-10-23 23:13:31 -0400

    Added shadow rendering back to the lightmap

---
+++ assets/shaders/source/shadowmap_ps.hlsl
@@ -0,0 +1,28 @@
+#include "bindless.hlsli"
+
+struct PSInput {
+    float4 position: SV_Position;
+};
+
+struct PSOutput {
+    float4 color: SV_Target0;
+};
+
+struct PassConstants {
+    float4x4 view_projection_matrix;
+    float3 camera_position;
+};
+
+ConstantBuffer<PassConstants> g_pass_consts : REGISTER_CBV(0, 0, 0);
+
+Texture2D<float4> g_textures[65536] : REGISTER_SRV(0, 0, 1);
+
+SamplerState g_aniso_repeat_sampler : REGISTER_SAMPLER(0, 0, 2);
+
+PSOutput main(PSInput input) {
+    PSOutput output;
+
+    output.color = float4(0.xxx, 1.);
+
+    return output;
+}
